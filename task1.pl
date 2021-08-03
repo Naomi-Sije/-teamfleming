449c66ffd0cd37ccb10a681eced6f39af14f96e8
alunga <alungamaxwell@gmail.com>
2021-08-04 01:47:30 +0300

    via perl

---
+++ task1.pl
@@ -0,0 +1,8 @@
+$Name = "Maxwell Booker ";
+$email = "alungamaxwell @ gmail.com ";
+$username = "@ Max ";
+$biostack = "drug development ";
+print $Name;
+print $email;
+print $username;
+print $biostack;
