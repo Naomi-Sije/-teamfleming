6d70b597ac87e7228fc754d0241bc312ea81e1ac
SaraElbesomy4 <sara2141997@gmail.com>
2021-08-03 11:34:29 +0200

    Added my script written with MATLAB

---
+++ SaraElbesomy_Task1.m
@@ -0,0 +1,12 @@
+clc
+clear
+
+name = 'Sara Elbesomy';
+email = 'sara.f.elbesomy@gmail.com';
+username = '@SaraElbesomy';
+biostack = 'Data science';
+
+disp(name);
+disp(email)
+disp(username)
+disp(biostack)
