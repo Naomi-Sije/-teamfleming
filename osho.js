d9be137457432cbe3a2d0592e2e444c0b015d478
Ridwan Osho <oshoridwan@gmail.com>
2021-08-04 00:42:54 +0100

    Stage 0 Task 1

---
+++ osho.js
@@ -0,0 +1,10 @@
+const details = {
+    name: "Osho Ridwan",
+    email: "oshoridwan@gmail.com",
+    slackUsername: "@Osho",
+    biostack: "Public Health and Genomic Epidemiology"
+}
+
+for (values in details){
+    console.log(details[values]);
+}
