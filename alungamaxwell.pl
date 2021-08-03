cffa4bf9117116869e278ee282626e99bb339175
Max <88286419+alunga20@users.noreply.github.com>
2021-08-03 15:11:24 +0300

    Add files via upload

---
+++ alungamaxwell.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#perl 5.26.1 
+# Hello World Program in Perl
+#
+$Name = "Maxwell Booker ";
+$email = "alungamaxwell @ gmail.com ";
+$username = "@ Max ";
+$biostack = "drug development ";
+print $Name;
+print $email;
+print $username;
+print $biostack;
