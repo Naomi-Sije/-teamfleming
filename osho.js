b3d87583856ecc9f2f4a2164a6b7ba835b2e3066
Ridwan Osho <oshoridwan@gmail.com>
2021-08-04 20:18:29 +0100

    Hamming Distance

--- osho.js
+++ osho.js
@@ -2,9 +2,26 @@ const details = {
     name: "Osho Ridwan",
     email: "oshoridwan@gmail.com",
     slackUsername: "@Osho",
-    biostack: "Public Health and Genomic Epidemiology"
+    biostack: "Public Health and Genomic Epidemiology",
+    twitterHandle: "@readone98"
+}
+
+function hammer(){
+    let slack = details.slackUsername.toLowerCase();
+    let twitter = details.twitterHandle;
+    let hamming = 0;
+
+    if ( slack !== twitter){
+        return 0;
+    }for (let i =0; i <= slack.length; i++){
+        if( twitter[i] !== slack[i]){
+            hamming++ 
+        };
+    };
+    return hamming;
 }
 
 for (values in details){
     console.log(details[values]);
 }
+console.log(hammer());
