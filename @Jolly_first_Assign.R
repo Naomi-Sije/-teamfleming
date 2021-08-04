16febfabc72de337c1edc9164ba16f0dc730951c
jolly-4 <88287059+jolly-4@users.noreply.github.com>
2021-08-04 06:48:26 +0100

    Add files via upload

---
+++ @Jolly_first_Assign.R
@@ -0,0 +1,5 @@
+HB = data.frame(name = ('adole jolly amoche'),
+                email = ('adolejolly@gmail.com'),
+                username = ('@jolly'),
+                biostack = ('genomicdata'))
+print (HB)
