c19b21597211b126915fa59604eb32c7bcd8d2ae
Samar Ghoneim <88295264+Samar-G@users.noreply.github.com>
2021-08-04 23:26:36 +0200

    First Task Solution in C++ by Samar Ghoneim

---
+++ SamarGhoneim-Task1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int hamming(string username, string twitter)
+{
+    int cnt=0;
+    int userLen = username.length();
+    int twiLen = twitter.length();
+    for(int i=0;i<userLen; i++)
+    {
+        if(userLen==twiLen)
+        {
+            if(username[i]!=twitter[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    return cnt;
+}
+
+void info(string name, string email, string username, string biostack, string twit)
+{
+    cout<< name<< ","<< email << ","<< username << ","<< biostack << ","<< twit << ","<< hamming(username,twit);
+}
+
+int main()
+{
+    string name = "Samar Ghoneim";
+    string email= "samarghonaim27@gmail.com";
+    string user = "@Samar";
+    string bio = "Drug Development";
+    string twit = "@SamG2";
+    info(name,email,user,bio, twit);
+    return 0;
+}
