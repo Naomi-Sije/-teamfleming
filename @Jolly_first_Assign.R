0441c2521c3392df429d435dd5121feafcc04d87
jolly-4 <88287059+jolly-4@users.noreply.github.com>
2021-08-04 18:38:11 +0100

    Delete @Jolly_first_Assign.R

--- @Jolly_first_Assign.R
+++
@@ -1,5 +0,0 @@
-HB = data.frame(name = ('adole jolly amoche'),
-                email = ('adolejolly@gmail.com'),
-                username = ('@jolly'),
-                biostack = ('genomicdata'))
-print (HB)
