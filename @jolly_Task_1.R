85f62b59192f6827d557af9f0aee09521adf26e9
jolly-4 <88287059+jolly-4@users.noreply.github.com>
2021-08-05 22:53:57 +0100

    Add files via upload

---
+++ @jolly_Task_1.R
@@ -0,0 +1,12 @@
+#Create vectors
+us = c('@', 'j', 'o', 'l', 'l', 'y')
+tw = c('@', 'd', 'o', 'l', 'e', 'y')
+sum(us != tw)
+HB = data.frame(name = ('adole jolly amoche'),
+                email = ('adolejolly@gmail.com'),
+                username = ('@jolly'),
+                biostack = ('genomicdata'),
+                twiter = ('@doley'),
+                hamm_dist = (2))
+print(HB)
+View(HB)
