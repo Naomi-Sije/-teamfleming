4d6057269996f044d6a3a2a43100529ff8bb55ae
Max <88286419+alunga20@users.noreply.github.com>
2021-08-06 23:51:09 +0300

    Update Task1_Max.pl

--- Task1_Max.pl
+++ Task1_Max.pl
@@ -1,11 +1,23 @@
 #!/usr/bin/perl
-$Name = "Maxwell Booker, ";
-$email = "alungamaxwell\@gmail.com, ";
-$username = "\@Max, ";
-$biostack = "drug development, ";
-$twitterhandle = "\@maxxisbooker";
+$Name = "Maxwell Booker,";
+$email = "alungamaxwell\@gmail.com,";
+$username = "\@Max,";
+$biostack = "drug development,";
+$twitterhandle = "\@maxxisbooker,";
+$c=0;
+for $i (1..length($username)){
+    if(length($username)==length($twitterhandle)){
+        if ($username[i]!=$twitterhandle[i]){
+        $c++;
+        }
+        else{
+            continue;
+        }
+    }
+}
 print $Name;
 print $email;
 print $username;
 print $biostack;
 print $twitterhandle;
+print $c;
