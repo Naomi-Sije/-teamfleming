f9b742a0765edd7b056477625e642b8687292a15
jolly-4 <88287059+jolly-4@users.noreply.github.com>
2021-08-06 22:44:40 +0100

    Add files via upload

---
+++ @_JOLLY_Task_1.R
@@ -0,0 +1,14 @@
+#Create vectors
+us = c('@', 'j', 'o', 'l', 'l', 'y')
+tw = c('@', 'd', 'o', 'l', 'e', 'y')
+#Hamming distance
+sum(us != tw)
+#Create CSV
+HB = data.frame(name = ('Adole Jolly Amoche,'),
+                email = ('adolejolly@gmail.com,'),
+                username = ('@jolly,'),
+                biostack = ('GenomicData,'),
+                twiter = ('@doley,'),
+                hamm_dist = (2))
+print(HB)
+View(HB)
