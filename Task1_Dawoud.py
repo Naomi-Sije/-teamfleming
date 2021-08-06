ae7e7d038a3056b1b1d8a0dead58b6a770e24301
Dawoud-U <71490918+DawoudUsman@users.noreply.github.com>
2021-08-07 00:54:03 +0100

    Update Task1_Dawoud.py

--- Task1_Dawoud.py
+++ Task1_Dawoud.py
@@ -3,6 +3,11 @@ email = "dawoudusman6@gmail.com,"
 slack_username = "@Dawoud,"
 biostack = "Transcriptomics,"
 twitter_handle = "@usman_dawoud,"
-hamming_distance = "0"
 
-print(name,email,slack_username,biostack,twitter_handle,hamming_distance)
+def hamming_distance(slack_username, twitter_handle):
+    if len(slack_username) != len(twitter_handle):
+        return "string lengths are not equal!"
+    else:
+        return sum(1 for (a,b) in zip(slack_username, twitter_handle))
+
+print(name, email, slack_username, biostack, twitter_handle, hamming_distance(slack_username, twitter_handle))
