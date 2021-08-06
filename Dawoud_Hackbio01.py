36d955b69542a1622ae1bdde06a9a80fc6cb3566
Dawoud-U <dawoudusman6@gmail.com>
2021-08-06 19:41:19 +0100

    just added a file

---
+++ Dawoud_Hackbio01.py
@@ -0,0 +1,8 @@
+name = "Usman Dawoud"
+email = "dawoudusman6@gmail.com"
+slack_username = "@Dawoud"
+biostack = "Transcriptomics"
+twitter_handle = "@usman_dawoud"
+hamming_distance = "0"
+
+print(name,email,slack_username,biostack,twitter_handle,hamming_distance)
