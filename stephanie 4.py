669f8a0f38d7820990d939f4aae3a419d526d0f6
SagaciousSteph53 <88290150+SagaciousSteph53@users.noreply.github.com>
2021-08-06 15:41:56 +0100

    wrong file

--- stephanie 4.py
+++
@@ -1,8 +0,0 @@
-name = 'Stephanie Ezirim'
-email = 'ezirimstephanie53@gmail.com'
-slackusername = '@stephanie'
-biostack = 'Medicinal chemistry and Chemoinformatics'
-twittername = '@stephanie ezirim'
-hamming= '0'
-
-print(name, email, slackusername, biostack, twittername, hamming )
