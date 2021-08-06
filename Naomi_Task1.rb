b34ac943355a20e305e3c06e43596f8090a2d7d5
Naomi-Sije <88307038+Naomi-Sije@users.noreply.github.com>
2021-08-06 04:12:34 -0700

    Update Naomi_Task1.rb

--- Naomi_Task1.rb
+++ Naomi_Task1.rb
@@ -1,6 +1,18 @@
-n = 'Naomi Ndifon, '
-e = 'ndifonnaomisijeokim@gmail.com, '
-s = '@Naomi, '
-b = 'Data Science'
+#stagezero
 
-print n, e, s, b
+n = 'Naomi Ndifon,';
+e = 'ndifonnaomisijeokim@gmail.com,';
+s = '@Naomi,';
+b = 'Data Science,';
+t = '@Siije,';
+
+#calculate_hamming_distance
+
+class Hamming
+  def self.compute(first, second)
+    first.chars.zip(second.chars).count { |(a, b)| a != b }
+  end
+end
+
+#final_output
+print n, e, s, b, t, (Hamming.compute(s,t));
