19989b7a821283e60faa86ad3fddefdad790d4cb
Samar Ghoneim <88295264+Samar-G@users.noreply.github.com>
2021-08-06 21:55:07 +0200

    Update Task1_Anne.r

--- Task1_Anne.r
+++ Task1_Anne.r
@@ -1,10 +1,6 @@
-
-R 
-
-
-> name <- "sopuluchukwu ugwu,"
-> email <- "sopuluchianne@gmail.com,"
-> username <- "@Anne,"
-> biostack <- "Data analysis,"
-> twiter <- "sopuluchi anne," 
-> print(c(name, email, username, biostack, twitter, sum(username!=twitter)))
+name <- "sopuluchukwu ugwu,"
+email <- "sopuluchianne@gmail.com,"
+username <- "@Anne,"
+biostack <- "Data analysis,"
+twiter <- "sopuluchi anne," 
+print(c(name, email, username, biostack, twitter, sum(username!=twitter)))
