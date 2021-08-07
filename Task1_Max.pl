25b99db2f0ffa65a6090e128654d3159bcc2ac7d
Max <88286419+alunga20@users.noreply.github.com>
2021-08-07 13:54:19 +0300

    Update Task1_Max.pl

--- Task1_Max.pl
+++ Task1_Max.pl
@@ -3,18 +3,20 @@ $Name = "Maxwell Booker,";
 $email = "alungamaxwell\@gmail.com,";
 $username = "\@Max,";
 $biostack = "drug development,";
-$twitterhandle = "\@maxxisbooker,";
+$twitterhandle = "\@Max,";
 $c=0;
+
 for $i (1..length($username)){
     if(length($username)==length($twitterhandle)){
         if ($username[i]!=$twitterhandle[i]){
         $c++;
         }
         else{
-            continue;
+            $c=$c;
         }
     }
 }
+
 print $Name;
 print $email;
 print $username;
