85a8e246967f95ceba8f14ff3b328af34fed8dde
Dawoud-U <71490918+DawoudUsman@users.noreply.github.com>
2021-08-07 14:27:00 +0100

    Update Task1_Dawoud.py

--- Task1_Dawoud.py
+++ Task1_Dawoud.py
@@ -8,6 +8,6 @@ def hamming_distance(slack_username, twitter_handle):
     if len(slack_username) != len(twitter_handle):
         return "string lengths are not equal!"
     else:
-        return sum(1 for (a,b) in zip(slack_username, twitter_handle))
+        return sum(1 for (a,b) in zip(slack_username, twitter_handle) if a!=b)
 
 print(name, email, slack_username, biostack, twitter_handle, hamming_distance(slack_username, twitter_handle))
