9a707c7546e9f68d1546a84eccb47c80c6af81bc
Sara <50876277+SaraElbesomy4@users.noreply.github.com>
2021-08-07 05:40:40 +0200

    change the code

--- @_JOLLY_Task_1.R
+++ @_JOLLY_Task_1.R
@@ -1,14 +1,10 @@
-#Create vectors
 us = c('@', 'j', 'o', 'l', 'l', 'y')
 tw = c('@', 'd', 'o', 'l', 'e', 'y')
-#Hamming distance
-sum(us != tw)
-#Create CSV
-HB = data.frame(name = ('Adole Jolly Amoche,'),
-                email = ('adolejolly@gmail.com,'),
-                username = ('@jolly,'),
-                biostack = ('GenomicData,'),
-                twiter = ('@doley,'),
-                hamm_dist = (2))
-print(HB)
-View(HB)
+
+name = ('Adole Jolly Amoche,')
+email = ('adolejolly@gmail.com,')
+username = ('@jolly,')
+biostack = ('GenomicData,')
+twiter = ('@doley,')
+hamm_dist = (sum(us != tw))
+cat(name,email,username,biostack,twiter,hamm_dist)
